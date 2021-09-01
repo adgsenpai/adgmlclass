d37d86dd09de90e749c9eca4c3b2edef7f49ba82
Ashlin Darius Govindasamy <adgrules@hotmail.com>
2021-09-02 00:12:55 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="adgmlclass",
+    version="0.0.1",
+    author="Ashlin Darius Govindasamy",
+    author_email="adgrules@hotmail.com",
+    url="https://www.adgstudios.co.za",
+    description="A class used for finding the best machine learning algorithm",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["click", "pytz"],
+    entry_points={"console_scripts": ["app = src.app:main"]},
+)
