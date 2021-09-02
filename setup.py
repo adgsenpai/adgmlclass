c14c2f1e6b973fe668655ee8054c860c177cb819
Ashlin Darius Govindasamy <adgrules@hotmail.com>
2021-09-02 20:31:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["click", "pytz","pandas","numpy","matplotlib","seaborn","scikit-learn"],
-    entry_points={"console_scripts": ["app = src.app:main"]},
-    
+    install_requires=["click", "pytz","pandas","numpy","matplotlib","seaborn","scikit-learn"],    
 )
