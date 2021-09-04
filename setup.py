c2c86228739377e1d45a815282c8ea39cb54a1dd
Ashlin Darius Govindasamy <adgrules@hotmail.com>
2021-09-04 17:08:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="adgmlclass",
-    version="0.0.5",
+    version="0.0.6",
     author="Ashlin Darius Govindasamy",
     author_email="adgrules@hotmail.com",
     url="https://www.adgstudios.co.za",
